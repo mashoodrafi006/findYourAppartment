5b67a5baefcffec3619020a7d313403e78df6a09
Mashood Rafi <pro@Mashood-MacBook-Pro-2019.local>
2021-09-01 12:34:12 +0500

    Change APP_LOCATION in confif file.

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ const config = {
     env: process.env.APP_ENV,
     port: process.env.PORT || 3000,
     gqlPort: process.env.GQL_PORT || 4000,
-    appLocation: process.env.APP_LOCATION || 'GERMANY',
+    appLocation: process.env.APP_LOCATION || 'FRANCE',
     mongoUrl: process.env.MONGO || 'mongodb://localhost:27017/homeLike',
 };
 
